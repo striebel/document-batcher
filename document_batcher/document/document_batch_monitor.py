8affeb5b1c5d472757f963081394bf7f0f32a3f5
Jacob Striebel <21368489+striebel@users.noreply.github.com>
2023-08-24 19:23:01 -0400

    removed remnants of processing slowdown restart feature

--- document_batcher/document/document_batch_monitor.py
+++ document_batcher/document/document_batch_monitor.py
@@ -16,59 +16,16 @@ class DocumentBatchMonitor():
         input_file                              : InputFile,
         logger                                  : Logger,
         time_budget_in_hours                    : int,
-        processing_slowdown_restart_window_size : int,
-        processing_slowdown_restart_factor      : float,
-        ########################################################################
-        #### parameters for testing
-        processing_slowdown_restart_test_run_test                 : bool,
-        processing_slowdown_restart_test_num_doc_batches_to_delay : int | None
     ) -> DocumentBatchMonitor:
-
         assert isinstance(input_file, InputFile)
         assert isinstance(logger, Logger)
         assert isinstance(time_budget_in_hours, int)
         assert 0 < time_budget_in_hours
-        assert isinstance(processing_slowdown_restart_window_size, int)
-        assert 0 < processing_slowdown_restart_window_size
-        assert isinstance(processing_slowdown_restart_factor, float)
-        assert 1.0 < processing_slowdown_restart_factor
-        if processing_slowdown_restart_test_run_test is True:
-            assert isinstance(
-                processing_slowdown_restart_test_num_doc_batches_to_delay, int
-            )
-            assert 0 <= processing_slowdown_restart_test_num_doc_batches_to_delay
-        else:
-            assert processing_slowdown_restart_test_run_test is False
-            assert processing_slowdown_restart_test_num_doc_batches_to_delay is None
         
-        ########################################################################
-        #### parameters for core functionality
-        self._input_file         = input_file
+        self._input_file           = input_file
         self._logger               = logger
         self._time_budget_in_hours = time_budget_in_hours
-        self._processing_slowdown_restart_window_size = \
-            processing_slowdown_restart_window_size
-        self._processing_slowdown_restart_factor = \
-            processing_slowdown_restart_factor
         
-        ########################################################################
-        #### parameters for testing processing slowdown restart functionality
-        self._processing_slowdown_restart_test_run_test = \
-            processing_slowdown_restart_test_run_test
-        self._processing_slowdown_restart_test_num_doc_batches_to_delay = \
-            processing_slowdown_restart_test_num_doc_batches_to_delay
-        
-        
-        ########################################################################
-        #### class instance private variables for processing slowdown restart
-        #### functionality
-        self._batch_sentence_rates_in_window = []
-        self._doc_batch_index_that_filled_window = -1
-        self._average_batch_sentence_rate_in_window_record_high = 0.0
-
-        ########################################################################
-        #### running totals for num docs, sents, words, and chars processed
-        #### so far
         self._n_docs  = 0
         self._n_sents = 0
         self._n_words = 0
@@ -87,225 +44,6 @@ class DocumentBatchMonitor():
         return self._index
 
 
-    def processing_slowdown_restart_test_run_test_is_true(
-        self : DocumentBatchMonitor
-    ) -> bool:
-
-        assert isinstance(self._processing_slowdown_restart_test_run_test, bool)
-
-        return self._processing_slowdown_restart_test_run_test
-
-
-    def processing_slowdown_restart_test_ready(
-        self                    : DocumentBatchMonitor,
-        current_doc_batch_index : int
-    ) -> None:
-        
-        assert self._processing_slowdown_restart_test_run_test_is_true()
-
-        if self._doc_batch_index_that_filled_window < 0:
-
-            return False
-
-        assert (
-            self._doc_batch_index_that_filled_window
-            <= current_doc_batch_index
-        )
-
-        num_doc_batches_delayed_so_far = \
-            current_doc_batch_index - 1 \
-            - self._doc_batch_index_that_filled_window
-        
-        assert 0 <= num_doc_batches_delayed_so_far
-
-        self._logger.debug(
-            dedent(
-                '''\
-                timing_handler:
-                DocumentBatchMonitor.processing_slowdown_restart_test_ready:
-                num_doc_batches_delayed_so_far: {0}'''
-            ).replace('\n', ' ').format(
-                num_doc_batches_delayed_so_far
-            )
-        )
-        
-        if (
-            num_doc_batches_delayed_so_far
-            < self._processing_slowdown_restart_test_num_doc_batches_to_delay
-        ):
-            
-            return False
-
-        else:
-
-            assert (
-                self._processing_slowdown_restart_test_num_doc_batches_to_delay
-                <= num_doc_batches_delayed_so_far
-            )
-
-            assert (
-                len(self._batch_sentence_rates_in_window)
-                == self._processing_slowdown_restart_window_size
-            )
-
-            assert 0.0 < self._average_batch_sentence_rate_in_window_record_high
-
-            return True
-    
-    
-    def processing_slowdown_restart_test_instigate_slowdown(
-        self                               : DocumentBatchMonitor,
-        current_doc_batch_index            : int,
-        num_sentences_in_current_doc_batch : int
-    ) -> None:
-
-        assert isinstance(self, DocumentBatchMonitor)
-        assert isinstance(current_doc_batch_index, int)
-        assert 0 <= current_doc_batch_index
-        assert isinstance(num_sentences_in_current_doc_batch, int)
-        assert 0 < num_sentences_in_current_doc_batch
-
-        assert self._processing_slowdown_restart_test_run_test_is_true()
-
-        assert self._processing_slowdown_restart_test_ready(
-            current_doc_batch_index = current_doc_batch_index
-        )
-
-        assert (
-            len(self._batch_sentence_rates_in_window)
-            == self._processing_slowdown_restart_window_size
-        )
-
-        assert 0.0 < self._average_batch_sentence_rate_in_window_record_high
-
-        upper_bound_on_average_batch_sentence_rate_to_achieve_restart = \
-            self._average_batch_sentence_rate_in_window_record_high \
-            / self._processing_slowdown_restart_factor
-
-        lower_bound_on_seconds_of_sleep_per_batch_to_achieve_restart = \
-            num_sentences_in_current_doc_batch \
-            / upper_bound_on_average_batch_sentence_rate_to_achieve_restart
-        
-        self._logger.debug(
-            dedent(
-                '''\
-                timing_handler:
-                DocumentBatchMonitor.processing_slowdown_restart_test_instigate_slowdown:
-                will now sleep for {0:.4} seconds'''
-            ).replace('\n', ' ').format(
-                lower_bound_on_seconds_of_sleep_per_batch_to_achieve_restart
-            )
-        )
-        
-        sleep(lower_bound_on_seconds_of_sleep_per_batch_to_achieve_restart)
-
-
-    def check_for_processing_slowdown(
-        self                    : DocumentBatchMonitor,
-        global_start_time       : datetime,
-        current_doc_batch_index : int
-    ) -> None:
-        
-        assert isinstance(self, DocumentBatchMonitor)
-        assert isinstance(global_start_time, datetime)
-        assert isinstance(current_doc_batch_index, int)
-        assert 0 <= current_doc_batch_index
-        
-        if (
-            len(self._batch_sentence_rates_in_window)
-            < self._processing_slowdown_restart_window_size
-        ):
-            return
-        
-        assert (
-            len(self._batch_sentence_rates_in_window)
-            == self._processing_slowdown_restart_window_size
-        )
-
-        average_batch_sentence_rate_in_window = \
-            sum(self._batch_sentence_rates_in_window) \
-            / self._processing_slowdown_restart_window_size
-        
-        if (
-            self._average_batch_sentence_rate_in_window_record_high
-            < average_batch_sentence_rate_in_window
-        ):
-            self._average_batch_sentence_rate_in_window_record_high = \
-                average_batch_sentence_rate_in_window
-            return
-        
-        if (
-            average_batch_sentence_rate_in_window
-            < (
-                self._average_batch_sentence_rate_in_window_record_high
-                / self._processing_slowdown_restart_factor
-            )
-        ):
-            self._logger.debug(
-                dedent(
-                    '''\
-                    timing_handler:
-                    DocumentBatchMonitor.check_for_processing_slowdown:
-                    slowdown has occurred, restarting'''
-                ).replace('\n', ' ')
-            )
-
-            newly_elapsed_seconds = int(
-                (datetime.now() - global_start_time).total_seconds()
-            )
-
-            if self.completion_report_file is not None:
-
-                assert isinstance(self.completion_report_file, str)
-
-                if os.path.isfile(self.completion_report_file):
-
-                    completion_report_file_obj = \
-                        open(self.completion_report_file, 'r')
-
-                    completion_report_str = \
-                        completion_report_file_obj.read().strip()
-
-                    assert 0 < len(completion_report_str)
-
-                    completion_report_dict = json.loads(completion_report_str)
-
-                    assert (
-                        'processing_slowdown_restart_num_restarts'
-                        in completion_report_dict
-                    )
-
-                    assert 0 < completion_report_dict[
-                        'processing_slowdown_restart_num_restarts'
-                    ]
-
-                    completion_report_file_obj.close()
-
-                else:
-
-                    completion_report_dict = {
-                        'processing_slowdown_restart_num_restarts': 0
-                    }
-
-                completion_report_dict[
-                    'processing_slowdown_restart_num_restarts'
-                ] += 1
-
-                completion_report_file_obj = \
-                    open(self.completion_report_file, 'w')
-
-                completion_report_file_obj.write(
-                    json.dumps(completion_report_dict)
-                )
-
-                completion_report_file_obj.close()
-
-            self.error_handler.recover(
-                doc_batch_start       = current_doc_batch_index,
-                newly_elapsed_seconds = newly_elapsed_seconds
-            )
-
-
     def post_batch_update(
         self          : DocumentBatchMonitor,
         batch_n_docs  : int,
@@ -318,7 +56,6 @@ class DocumentBatchMonitor():
         METHOD_NAME = 'document.document_batch_monitor: ' \
             + 'DocumentBatchMonitor.post_batch_update:'
         
-
         ########################################################################
         #### validate method invocation preconditions
         
@@ -339,22 +76,6 @@ class DocumentBatchMonitor():
         assert 0 <= self._n_secs
         assert isinstance(self._index, int)
         assert 0 <= self._index
-
-        assert isinstance(self._processing_slowdown_restart_window_size, int)
-        assert 0 < self._processing_slowdown_restart_window_size
-        assert isinstance(self._processing_slowdown_restart_factor, float)
-        assert 1.0 < self._processing_slowdown_restart_factor
-        assert isinstance(self._batch_sentence_rates_in_window, list)
-        assert 0 <= len(self._batch_sentence_rates_in_window)
-        assert (
-            len(self._batch_sentence_rates_in_window)
-            <= self._processing_slowdown_restart_window_size
-        )
-        for batch_sentence_rate in self._batch_sentence_rates_in_window:
-            assert isinstance(batch_sentence_rate, float)
-            assert 0.0 < batch_sentence_rate
-        assert isinstance(self._average_batch_sentence_rate_in_window_record_high, float)
-        assert 0.0 <= self._average_batch_sentence_rate_in_window_record_high
         
         
         ########################################################################
@@ -373,6 +94,8 @@ class DocumentBatchMonitor():
         
         
         
+        #######################################################################
+        #### update accumulators
         
         self._n_docs  += batch_n_docs
         self._n_sents += batch_n_sents
@@ -381,6 +104,10 @@ class DocumentBatchMonitor():
         self._n_secs  += batch_n_secs
         
         
+
+        #######################################################################
+        #### calcuate progress statistics
+
         total_hours   = math.floor(self._n_secs / 60 / 60)
         total_minutes = math.floor(self._n_secs / 60) - (total_hours * 60)
         total_seconds = self._n_secs - (total_hours * 60 * 60) - (total_minutes * 60)
@@ -397,60 +124,8 @@ class DocumentBatchMonitor():
         batch_sent_rate = batch_n_sents / batch_n_secs
         batch_word_rate = batch_n_words / batch_n_secs
         batch_char_rate = batch_n_chars / batch_n_secs
-    
         
-
-        ########################################################################
-        #### processing slowdown restart: maintain necessary statistics
-
-        if (
-            len(self._batch_sentence_rates_in_window)
-            < self._processing_slowdown_restart_window_size - 1
-        ):
-            assert -1 == self._doc_batch_index_that_filled_window
-
-            self._batch_sentence_rates_in_window.append(batch_sent_rate)
-
-        elif (
-            len(self._batch_sentence_rates_in_window)
-            == self._processing_slowdown_restart_window_size - 1
-        ):
-            assert -1 == self._doc_batch_index_that_filled_window
-
-            self._doc_batch_index_that_filled_window = self._index
-
-            self._batch_sentence_rates_in_window.append(batch_sentence_rate)
-
-        elif (
-            len(self._batch_sentence_rates_in_window)
-            == self._processing_slowdown_restart_window_size
-        ):
-            assert 0 <= self._doc_batch_index_that_filled_window
-
-            self._batch_sentence_rates_in_window.append(batch_sentence_rate)
-
-            self._batch_sentence_rates_in_window.pop(0)
-
-            assert (
-                len(self._batch_sentence_rates_in_window)
-                == self._processing_slowdown_restart_window_size
-            )
-
-        else:
-
-            assert (
-                self._processing_slowdown_restart_window_size
-                < len(self._batch_sentence_rates_in_window)
-            )
-
-            assert False, \
-                dedent(
-                    '''\
-                    {0}
-                    processing_slowdown_restart_window was overfilled'''
-                ).replace('\n', ' ').format(
-                    METHOD_NAME
-                )
+        
         
         
         time_budget_in_seconds = self._time_budget_in_hours * 60 * 60
@@ -480,11 +155,11 @@ class DocumentBatchMonitor():
         estimated_total_time_in_seconds = self._n_secs + estimated_time_remaining_in_seconds
     
         estimated_fraction_of_time_budget_needed = estimated_total_time_in_seconds / time_budget_in_seconds
-    
-    
+        
+        
         
         ########################################################################
-        #### log doc processing progress information
+        #### log progress statistics
         
         self._logger.info(
             dedent(
